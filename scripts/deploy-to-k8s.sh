fd774b15ee94af750d4a326d993e5b6dc29b3b78
student <kubernetes20@git.com>
2020-10-15 05:11:29 +0100

    PVC

--- scripts/deploy-to-k8s.sh
+++ scripts/deploy-to-k8s.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
+kubectl apply -f deployment/mysql-pv.yaml
+kubectl apply -f deployment/mysql-pvc.yaml
+
 kubectl apply -f deployment/pages-namespace.yaml
 kubectl apply -f deployment/pages-config.yaml
 kubectl apply -f deployment/pages-service.yaml
@@ -11,3 +14,4 @@ kubectl apply -f deployment/mysql-service.yaml
 kubectl apply -f deployment/mysql-deployment.yaml
 kubectl apply -f deployment/flyway-configmap.yaml
 kubectl apply -f deployment/flyway-job.yaml
+
